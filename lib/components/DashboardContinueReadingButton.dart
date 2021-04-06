87760bffdf0f41a3d5cf6a019caa3c8e531377c3
UcGeorge <petegeorge20005@gmail.com>
2021-04-06 02:48:25 +0100

    Created continue-reading button component

---
+++ lib/components/DashboardContinueReadingButton.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CRB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 111,
+      height: 25,
+      decoration: BoxDecoration(
+        color: Color(0xFF151311),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        border: Border.all(
+          color: Color(0xFFEAEAEA),
+          width: 1,
+        )
+      ),
+      child: Center(
+        child: Text(
+          'continue reading',
+          style: TextStyle(
+            fontSize: 9,
+            fontWeight: FontWeight.w300,
+            color: Color(0xFFEAEAEA),
+            letterSpacing: 1
+          ),
+        ),
+      ),
+    );
+  }
+}
