702841215f04e8d7010b5d2b0a300428dd50c163
UcGeorge <petegeorge20005@gmail.com>
2021-04-15 14:29:51 +0100

    Changed initial route / home to point to the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:profoundgodlibrary/views/Chapter.dart';
 import 'package:profoundgodlibrary/views/Novel.dart';
+import 'package:profoundgodlibrary/views/splash.dart';
 import 'bloc/theme_bloc.dart';
 import 'constants/constants.dart';
 import 'package:profoundgodlibrary/views/dashboard.dart';
+// import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 /// Custom [BlocObserver] which observes all bloc and cubit instances.
 class SimpleBlocObserver extends BlocObserver {
@@ -27,7 +30,19 @@ class SimpleBlocObserver extends BlocObserver {
   }
 }
 
-void main(){
+void connect() async {
+  const BASE = 'http://127.0.0.1:5000';
+  String endpoint = '/user/uche';
+
+  var url = Uri.parse(BASE + endpoint);
+  var response = await http.get(url);
+
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+}
+
+void main() {
+  // connect();
   Bloc.observer = SimpleBlocObserver();
   runApp(ProfoundGodLibrary());
 }
@@ -41,16 +56,14 @@ class ProfoundGodLibrary extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'PG Library',
         theme: ThemeData(
-          fontFamily: 'Fira Sans',
-          primaryColor: Color(0xFF151311),
-          textTheme: TextTheme(
-            overline: TextStyle(
+            fontFamily: 'Fira Sans',
+            primaryColor: Color(0xFF151311),
+            textTheme: TextTheme(
+                overline: TextStyle(
               fontFamily: 'Fira Sans',
               color: Color(0xFF151311),
-            )
-          )
-        ),
-        initialRoute: HOME_SCREEN,
+            ))),
+        home: SplashScreen(),
         routes: <String, WidgetBuilder>{
           HOME_SCREEN: (BuildContext context) => Dashboard(),
           NOVEL: (BuildContext context) => NovelView(),
