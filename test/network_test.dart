4753e4c0ffda1681ff6611eb4f2ec740f476c57f
UcGeorge <petegeorge20005@gmail.com>
2021-04-15 14:28:39 +0100

    Create network_test.dart

---
+++ test/network_test.dart
@@ -0,0 +1,14 @@
+// ignore: unused_import
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+const BASE = 'http://127.0.0.1:5000';
+String endpoint = '/user/uche';
+
+void main() async {
+  var url = Uri.http(BASE, endpoint);
+  var response = await http.get(url);
+
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+}
